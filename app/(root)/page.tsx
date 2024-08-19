738a1d60dfe227193172fa7cef9ce57fd6f2c3b7
Sean Currlin <sean.currlin@gmail.com>
2024-08-19 17:02:28 -0400

    update home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,3 +1,13 @@
+import AddDocumentBtn from '@/components/AddDocumentBtn';
+import { DeleteModal } from '@/components/DeleteModal';
+import Header from '@/components/Header'
+import Notifications from '@/components/Notifications';
+import { Button } from '@/components/ui/button'
+import { getDocuments } from '@/lib/actions/room.actions';
+import { dateConverter } from '@/lib/utils';
+import { SignedIn, UserButton } from '@clerk/nextjs'
+import { currentUser } from '@clerk/nextjs/server';
+import Image from 'next/image';
 import Link from 'next/link';
 import { redirect } from 'next/navigation';
 
