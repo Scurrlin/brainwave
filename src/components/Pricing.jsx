6c782f69df8c0a22afc02b0b69f39fbbe8f2f35e
Sean Currlin <sean.currlin@gmail.com>
2024-09-14 08:37:56 -0400

    remove extra hrefs

--- src/components/Pricing.jsx
+++ src/components/Pricing.jsx
@@ -41,7 +41,6 @@ const Pricing = () => {
         <div className="flex justify-center mt-10">
           <a
             className="text-xs font-code font-bold tracking-wider uppercase border-b"
-            href="/pricing"
           >
             See the full details
           </a>
